d6b8284fac60b2513385fb539bbe22bdb7eb4cb4
Sky West <sshirasshmu@gmail.com>
2020-12-30 16:45:46 +0200

    commit

--- flowchartCode.js
+++ flowchartCode.js
@@ -1,30 +1,75 @@
 $(document).ready(function () {
-    $(".flowchart-container .plate").css("width", $(".flowchart-container .plate").css("height"));
-    $(".flowchart-container .connection-plate").css("width", $(".flowchart-container .connection-plate").css("height"));
+    sizePlates();
 
+    $(".draggable").on("mouseover", function () {
+        $("#draggable-name").val($(this).attr("id"));
+    });
+
+    $(".draggable").on("mouseout", function () {
+        $("#draggable-name").val("");
+    });
+    
     $(".draggable").draggable({
-        revert: "invalid", // when not dropped, the item will revert back to its initial position
+        revert: "invalid",
         containment: "document",
-        // helper: "clone",
+        helper: "clone",
         cursor: "grab"
     });
 
-    $(".plate").droppable({
-        accept: ".draggable",
-        // classes: {
-        //     "ui-droppable-active": "ui-state-highlight"
-        // },
+    $(".channel-plate").droppable({
+        accept: ".channel",
         drop: function (event, ui) {
-            deleteImage(ui.draggable);
+            accept($(this), ui.draggable);
+            ui.helper.data('dropped', true);
+        }
+    });
+
+    $(".connection-plate").droppable({
+        accept: ".connector",
+        drop: function (event, ui) {
+            accept($(this), ui.draggable);
+            ui.helper.data('dropped', true);
         }
     });
 });
 
-function deleteImage($item) {
-    $(".plate").html("");
-    $item.css({
+function sizePlates() {
+    $(".flowchart-container .plate").each(function () {
+        $(this).width($(this).height());
+    });
+}
+
+function accept($plate, $draggable) {
+    $plate.html("");
+    // $($draggable).clone().css({
+    //     "width": $plate.width(),
+    //     "height": $plate.height(),
+    //     "margin": "0",
+    //     "top": "0",
+    //     "left": "0"
+    // }).appendTo($plate);
+    var $clone = $($draggable).clone();
+    $clone.css({
+        "width": $plate.width(),
+        "height": $plate.height(),
+        "margin": "0",
         "top": "0",
         "left": "0"
     });
-    $(".plate").append($item);
+    $clone.appendTo($plate);
+    $clone.draggable({
+        revert: false,
+        containment: "document",
+        cursor: "grab",
+        start: function (event, ui) {
+            ui.helper.data('dropped', false);
+        },
+        stop: function (event, ui) {
+            // alert('stop: dropped=' + ui.helper.data('dropped'));
+            if (!ui.helper.data('dropped')) {
+                $(this).remove();
+            }
+            // Check value of ui.helper.data('dropped') and handle accordingly...
+        }
+    });
 }
